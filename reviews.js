aafbaf40e584dbd2672bbb80e1ec3049d93ccd00
Sakthi130 <132883494+Sakthi130@users.noreply.github.com>
2025-05-16 14:40:45 +0530

    Update reviews.js

--- reviews.js
+++ reviews.js
@@ -1,5 +1,5 @@
 // reviews.js
-const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT-aDDMSlRPZWybuzjfxB0ip_F-YuNiITedkjYSYQGCV7amDV5kqihqQ7ajZFxwPJb59wxxpkiVblAf/pub?gid=1831553687&single=true&output=csv';
+const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTaYVCNVbwusD3ykt6zDrfNQuNTVmk9W2CVxodvOvvNbOL2qH5FPFEW6jJ-Ld6JXkt7m8b2lqKlZMfv/pub?gid=1831553687&single=true&output=csv';
 
 function parseName(email) {
   const namePart = email.split('@')[0];
